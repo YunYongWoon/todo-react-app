a9c20431aced814f2dc2b3e28e1edd6e9fe5823e
YongWoon_Yun <yuyun0124@gmail.com>
2021-12-16 18:02:08 +0900

    서비스 통합

--- src/App.js
+++ src/App.js
@@ -1,41 +1,94 @@
 import React from "react";
 import Todo from "./Todo";
 import AddTodo from "./AddTodo";
-import {Paper, List, Container} from "@material-ui/core";
+import { Paper, List, Container } from "@material-ui/core";
 import './App.css';
+import { call } from './service/ApiService';
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
-      items : [],
+      items: [],
     };
   }
-
+  /*
   add = (item) => {
     const thisItems = this.state.items;
     item.id = "ID-" + thisItems.length;
-    item.done=false;
+    item.done = false;
     thisItems.push(item);
-    this.setState({items: thisItems});
+    this.setState({ items: thisItems });
     console.log("items : ", this.state.items);
   }
 
   delete = (item) => {
     const thisItems = this.state.items;
     console.log("Before Update Items : ", this.state.items);
-    const newItems = thisItems.filter(e=> e.id!== item.id);
-    this.setState({items : newItems}, () =>{
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
       console.log("Update Items : ", this.state.items);
     });
   }
 
-  render(){
-    var todoItems = this.state.items.length>0 && (
-      <Paper style={{margin:16}}>
+  componentDidMount() {
+    const requestOptions = {
+      method: "GET",
+      headers: { "Content-Type": "application/json" }
+    };
+
+    fetch("http://localhost:8080/todo", requestOptions)
+      .then((response) => response.json())
+      .then(
+        (response) => {
+          this.setState({
+            items: response.data,
+          });
+        },
+        (error) => {
+          this.setState({
+            error,
+          });
+        }
+      );
+  }
+  */
+
+  componentDidMount() {
+    call("/todo", "GET", null).then((response) =>
+      this.setState({ items: response.data })
+    );
+  }
+
+  add = (item) => {
+    call("/todo", "POST", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
+  delete = (item) => {
+    call("/todo", "DELETE", item).then((response) =>
+      this.setState({ items: response.data })
+    );
+  };
+
+  update = (item) => {
+    call("/todo","PUT", item).then((response)=>
+      this.setState({items: response.data})
+    )
+  }
+
+  render() {
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{ margin: 16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete = {this.delete}/>
+            <Todo 
+            item={item} 
+            key={item.id} 
+            delete={this.delete} 
+            update={this.update}
+            />
           ))}
         </List>
       </Paper>
@@ -44,7 +97,7 @@ class App extends React.Component{
     return (
       <div className="App">
         <Container maxWidth="md">
-          <AddTodo add={this.add}/>
+          <AddTodo add={this.add} />
           <div className="TodoList">{todoItems}</div>
         </Container>
       </div>
